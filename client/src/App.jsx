e867c72b29be5150a2a7ca06434a02e9d2abb871
mehdi-cse-bd <mehdi.cse.bd@gmail.com>
2024-03-01 21:49:25 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+
 const App = () => {
   return (
-    <div>
-      <h1>Good</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,15 @@
+const Error = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center text-success my-5">
+            Error: Page Not Available
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Error;
